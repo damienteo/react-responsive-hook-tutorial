d3239d736a17812fda9574fa89a8ba36bb580c88
damienteo <ksteo89@gmail.com>
2019-09-06 10:57:26 +0800

    Update App with WindowsDimensionsProvider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Content from "./components/Content";
+import WindowDimensionsProvider from "./components/WindowsDimensionsProvider/index";
+// import items from "./data.json";
+import "bulma/css/bulma.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WindowDimensionsProvider>
+      <div className="App">{/* <Content item={items}></Content> */}</div>
+    </WindowDimensionsProvider>
   );
 }
 
